d8af381816a7d4c554c76dd7079d161f9e4db632
Fujinoka <68637268+Fujinoka@users.noreply.github.com>
2021-04-15 09:36:58 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
-
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -24,23 +22,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-
   LatLng _initialcameraposition = LatLng(20.5937, 78.9629);
   late GoogleMapController _controller;
   Location _location = Location();
 
-  void _onMapCreated(GoogleMapController _cntlr)
-  {
+  void _onMapCreated(GoogleMapController _cntlr) {
     _controller = _cntlr;
     _location.onLocationChanged.listen((l) {
       _controller.animateCamera(
         CameraUpdate.newCameraPosition(
-          CameraPosition(target: LatLng(l.latitude, l.longitude),zoom: 15),
+          CameraPosition(target: LatLng(20.5937, 78.9629), zoom: 15),
         ),
       );
     });
   }
-
 
   @override
   Widget build(BuildContext context) {
@@ -51,7 +46,8 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Stack(
           children: [
             GoogleMap(
-              initialCameraPosition: CameraPosition(target: _initialcameraposition),
+              initialCameraPosition:
+                  CameraPosition(target: _initialcameraposition),
               mapType: MapType.normal,
               onMapCreated: _onMapCreated,
               myLocationEnabled: true,
@@ -61,6 +57,4 @@ class _MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-
-
 }
